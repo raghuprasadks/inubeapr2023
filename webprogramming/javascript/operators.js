0bc54d126504ddc20c212b8da1447bc7a3a6b43f
raghuprasadks <prasadraghuks@gmail.com>
2023-04-13 15:25:57 +0530

    bs

---
+++ webprogramming/javascript/operators.js
@@ -0,0 +1,19 @@
+/**
+ * Arithmetic - +,-,*,/,%
+ * Relational - >,<,>=,==,===,!=
+ * Logical - &&,||,!
+ * 
+ *  */
+
+n1=100
+n2=200
+result = n1+n2
+console.log(n1+ ' + ' +n2+' = '+result)
+n3='100'
+result = n1===n3
+console.log(' n1===n3 '+result)
+n4=400
+result = (n1<n2) && (n4>n2)
+console.log(' (n1<n2) && (n4>n2) '+result)
+
+
