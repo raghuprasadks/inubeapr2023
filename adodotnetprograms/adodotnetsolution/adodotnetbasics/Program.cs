24d11851b08c50d1e7cabd5fea30ab6436c7bf43
raghuprasadks <prasadraghuks@gmail.com>
2023-04-14 18:27:39 +0530

    ado net

---
+++ adodotnetprograms/adodotnetsolution/adodotnetbasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adodotnetbasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ adodotnetprograms/adonetwindowssolution/adonetwindows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonetwindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ado.NET Demo");
+            Console.ReadKey();
+        }
+    }
+}
