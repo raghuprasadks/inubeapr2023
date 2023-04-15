9c204e83c8f24872934be193ea1a5208a8a2ece3
raghuprasadks <prasadraghuks@gmail.com>
2023-04-15 11:00:24 +0530

    updated

--- adodotnetprograms/adonetwindowssolution/adonetwindows/Program.cs
+++ adodotnetprograms/adonetwindowssolution/adonetwindows/Program.cs
@@ -12,8 +12,14 @@ namespace adonetwindows
         static void Main(string[] args)
         {
             Console.WriteLine("Ado.NET Demo");
+            //   readStudent();
+            // string ConString = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True";
+            createDataBase();
+            Console.ReadKey();
+        }
 
-           // string ConString = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True";
+        static void readStudent()
+        {
             String ConString = "data source=inubedb.cdtqd6jgia7i.ap-south-1.rds.amazonaws.com;initial catalog=TestDB;uid=admin;pwd=kaushalyaat2017;TrustServerCertificate=True";
 
             SqlConnection con = new SqlConnection(ConString);
@@ -25,7 +31,31 @@ namespace adonetwindows
             {
                 Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
             }
-            Console.ReadKey();
+        }
+
+        static void createDataBase()
+        {
+            String ConString = "data source=inubedb.cdtqd6jgia7i.ap-south-1.rds.amazonaws.com;initial catalog=TestDB;uid=admin;pwd=kaushalyaat2017;TrustServerCertificate=True";
+
+            SqlConnection con = new SqlConnection(ConString);
+
+            string query = "Create Database ComputerShop";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database Created Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+               // Console.ReadKey();
+            }
         }
     }
 }
