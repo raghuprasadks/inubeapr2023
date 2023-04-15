6edc3a770aaf8db21adcce9bc8f179ba11337224
raghuprasadks <prasadraghuks@gmail.com>
2023-04-15 12:16:06 +0530

    updated

--- adodotnetprograms/adonetwindowssolution/adonetwindows/Program.cs
+++ adodotnetprograms/adonetwindowssolution/adonetwindows/Program.cs
@@ -14,7 +14,10 @@ namespace adonetwindows
             Console.WriteLine("Ado.NET Demo");
             //   readStudent();
             // string ConString = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True";
-            createDataBase();
+            //  createDataBase();
+            //createTable();
+            //insertRecord();
+            insertRecordWithParameter();
             Console.ReadKey();
         }
 
@@ -57,5 +60,102 @@ namespace adonetwindows
                // Console.ReadKey();
             }
         }
+
+        static void createTable()
+        {
+
+            String ConString = "data source=inubedb.cdtqd6jgia7i.ap-south-1.rds.amazonaws.com;initial catalog=TestDB;uid=admin;pwd=kaushalyaat2017;TrustServerCertificate=True";
+
+            SqlConnection con = new SqlConnection(ConString);
+
+          //  SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security=True");
+            string query =
+            @"CREATE TABLE dbo.Products
+                (
+                    ID int IDENTITY(1,1) NOT NULL,
+                    Name nvarchar(50) NULL,
+                    Price nvarchar(50) NULL,
+                    Date datetime NULL,
+                    CONSTRAINT pk_id PRIMARY KEY (ID)
+                );";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+
+        static void insertRecord()
+        {
+            String ConString = "data source=inubedb.cdtqd6jgia7i.ap-south-1.rds.amazonaws.com;initial catalog=TestDB;uid=admin;pwd=kaushalyaat2017;TrustServerCertificate=True";
+
+            SqlConnection con = new SqlConnection(ConString);
+
+
+            // SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security=True");
+            string query = "INSERT INTO Products (Name,Price,Date) VALUES('LED Screen','$120','27 January 2017')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Records Inserted Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+
+        static void insertRecordWithParameter()
+        {
+            //  SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security=True");
+            //Replaced Parameters with Value
+
+            String ConString = "data source=inubedb.cdtqd6jgia7i.ap-south-1.rds.amazonaws.com;initial catalog=TestDB;uid=admin;pwd=kaushalyaat2017;TrustServerCertificate=True";
+
+            SqlConnection con = new SqlConnection(ConString);
+
+            string query = "INSERT INTO Products (Name, Price, Date) VALUES(@Name, @Price, @Date)";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            //Pass values to Parameters
+            cmd.Parameters.AddWithValue("@Name", "USB Keyboard");
+            cmd.Parameters.AddWithValue("@Price", "$20");
+            cmd.Parameters.AddWithValue("@Date", "25 May 2017");
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Records Inserted Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
     }
-}
+    }
+
