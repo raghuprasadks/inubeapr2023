0ced61d9f744ed285293ccf1a2a5f8970cdf62b3
raghuprasadks <prasadraghuks@gmail.com>
2023-04-20 14:24:40 +0530

    updated

---
+++ webapidemo/coreapidemo/coreapidemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreapidemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
