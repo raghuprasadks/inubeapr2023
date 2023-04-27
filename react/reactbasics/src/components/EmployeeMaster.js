26f96c8354ade6e9d0f5d18634b2627e00bfb12d
raghuprasadks <prasadraghuks@gmail.com>
2023-04-27 09:54:05 +0530

    updated

--- react/reactbasics/src/components/EmployeeMaster.js
+++ react/reactbasics/src/components/EmployeeMaster.js
@@ -8,6 +8,10 @@ const EmployeeMaster=()=>{
     const [desg,setDesg]=useState('')
     const [sal,setSal]=useState('')
     const [emplist,setEmplist]=useState([])
+
+    const [isEdit,setIsEdit]=useState(true)
+    const [isAdd,setIsAdd]=useState(false)
+    
     
     const addEmployee=()=>{
         const emp = {code:code,name:name,dept:dept,desg:desg,sal:sal}
@@ -20,11 +24,15 @@ const EmployeeMaster=()=>{
         setEmplist(updEmpList);
        // setEmplist([...emplist,emp]);       
        console.log(updEmpList)  
-       setCode('')
-       setName('')
-       setDept('')
-       setDesg('')
-       setSal('')   
+       clearData()
+    }
+
+    const clearData=()=>{
+        setCode('')
+        setName('')
+        setDept('')
+        setDesg('')
+        setSal('') 
     }
 
     const deleteEmployee=(code)=>{
@@ -32,13 +40,41 @@ const EmployeeMaster=()=>{
         const afterdelete = emplist.filter((emp)=>emp.code!=code)
         setEmplist(afterdelete)
     }
+
+    const updateEmployee=(emptoupdate)=>{
+        console.log('emp to update',emptoupdate)
+        //const employeetobeupdated = emplist.filter((emp)=>emp.code!=code)
+        //setEmplist(afterdelete)
+        setCode(emptoupdate.code)
+        setName(emptoupdate.name)
+        setDept(emptoupdate.dept)
+        setDesg(emptoupdate.desg)
+        setSal(emptoupdate.sal)  
+        setIsEdit(false) 
+        setIsAdd(true)
+    }
+
+    const editEmployee=()=>{
+        const editedemployee = {code:code,name:name,dept:dept,desg:desg,sal:sal}
+        console.log("edit Employee",editedemployee)
+        
+        const afteredit=emplist.map((emp)=>emp.code==editedemployee.code?editedemployee:emp)
+        console.log("after edit",afteredit)
+        
+        setEmplist(afteredit)
+        clearData()
+        setIsEdit(true) 
+        setIsAdd(false)
+    }
+
     const emplistfordisplay=emplist.map((emp)=><tr key={emp.code}>
         <td>{emp.code}</td>
         <td>{emp.name}</td>
         <td>{emp.dept}</td>
         <td>{emp.desg}</td>
         <td>{emp.sal}</td>
-        <td><button onClick={()=>deleteEmployee(emp.code)}>Delete</button></td>
+        <td><button onClick={()=>deleteEmployee(emp.code)} >Delete</button></td>
+        <td><button onClick={()=>updateEmployee(emp)}>Update</button></td>
     </tr>)
 
     return(
@@ -50,7 +86,8 @@ const EmployeeMaster=()=>{
             <input value={dept} onChange={(e)=>setDept(e.target.value)} placeholder='Enter Department'></input><br/>
             <input value={desg} onChange={(e)=>setDesg(e.target.value)} placeholder='Enter Designation'></input><br/>
             <input value={sal} onChange={(e)=>setSal(e.target.value)} placeholder='Enter Salary'></input><br/>
-            <button onClick={addEmployee}>Add Employee</button>
+            <button onClick={addEmployee} disabled={isAdd}>Add Employee</button>
+            <button onClick={editEmployee} disabled={isEdit}>Edit Employee</button>
 
         </div>
         <div>
