f0b34d210a8832dc712df2a975cd6536c53dd0d5
raghuprasadks <prasadraghuks@gmail.com>
2023-04-29 14:47:07 +0530

    updated

--- react/contextdemo/src/App.js
+++ react/contextdemo/src/App.js
@@ -1,16 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import Component1 from './components/Component1';
-import Component2 from './components/Component2';
-import Component3 from './components/Component3';
+import { useState, createContext, useContext } from "react";
 
+const UserContext = createContext();
 
 function App() {
+  const [user, setUser] = useState("Jesse Hall");
   return (
+    
     <div>
-      <Component1></Component1>
+      <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2></Component2>
+      </UserContext.Provider>
     </div>
   );
 }
 
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
 export default App;
